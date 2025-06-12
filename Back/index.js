aa2571c9913492b5fc638b7424f4e9285adceea4
Devdot05 <olawoledotun85@gmail.com>
2025-06-12 12:08:15 +0100

    cors updated

--- Back/index.js
+++ Back/index.js
@@ -15,7 +15,15 @@ app.use((req, res, next) => {
 });
 
  
-app.use(cors())
+const corsOptions = {
+  origin: 'https://task-tracker-tiz5.vercel.app',
+  // credentials: true, // needed if using cookies/sessions
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({limit:"500mb"}))
 app.use(express.urlencoded({extended:true, limit:"500mb"}));
 
