34a2867ecb5f0fb0848e0b497076c69095121503
Devdot05 <olawoledotun85@gmail.com>
2025-06-12 15:41:59 +0100

    not allow resolve

--- Back/index.js
+++ Back/index.js
@@ -38,8 +38,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({limit:"500mb"}))
-app.use(express.urlencoded({extended:true, limit:"500mb"}));
+app.use(express.json())
+app.use(express.urlencoded());
 
 // app.use(session({
 //   secret: SESSION_SECRET,
